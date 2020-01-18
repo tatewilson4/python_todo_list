97f32bc3db394f4bbfe071cd87e723d6ef8fa639
Tate Wilson <tatewilson4@gmail.com>
2020-01-18 13:17:58 +0700

    created the model, linked admin iterface, made views and urls, created basic html

--- mysite/todos/views.py
+++ mysite/todos/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from todos.models import Todo
+
+class TodoList(ListView):
+    model = Todo
+
+class TodoView(DetailView):
+    model = Todo
+
+class TodoCreate(CreateView):
+    model = Todo
+    fields = ['item']
+    success_url = reverse_lazy('todo_list')
+
+class TodoUpdate(UpdateView):
+    model = Todo
+    fields = ['item']
+    success_url = reverse_lazy('todo_list')
+
+class TodoDelete(DeleteView):
+    model = Todo
+    success_url = reverse_lazy('todo_list')
